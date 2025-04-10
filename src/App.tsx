81c520e5434bc032a442a22729467d09a8ca8df1
Pixoll <pixoll.gamer@gmail.com>
2025-04-10 17:24:57 -0400

    Fix ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { JSX } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-export default function App() {
+export default function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
