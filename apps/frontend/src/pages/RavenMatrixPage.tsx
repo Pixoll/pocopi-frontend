597660b6a78c40c29e587aa2f77a7cf1fa1b3d2f
Pixoll <pixoll.gamer@gmail.com>
2025-04-24 19:49:11 -0400

    feat: make buttons functional

--- apps/frontend/src/pages/RavenMatrixPage.tsx
+++ apps/frontend/src/pages/RavenMatrixPage.tsx
@@ -5,10 +5,14 @@ import styles from "./RavenMatrixPage.module.css";
 const PROTOCOL = "control";
 
 export function RavenMatrixPage() {
+  const [phase, setPhase] = useState<number>(0);
+  const [question, setQuestion] = useState<number>(0);
   const [selected, setSelected] = useState<string>("");
 
-  // TODO should be props
-  const { img, options: tempOptions } = config.protocols[PROTOCOL].phases[0];
+  const { phases } = config.protocols[PROTOCOL];
+  const { questions } = phases[phase];
+  const { img, options: tempOptions } = questions[question];
+
   const options = tempOptions.map(option => {
     const base64 = option.src.split(";")[1].slice(7);
     const half = Math.round(base64.length / 2);
@@ -16,8 +20,37 @@ export function RavenMatrixPage() {
 
     return { id, ...option };
   });
+
   const optionsColumns = Math.ceil(options.length / 2);
   const isOptionsOdd = options.length % 2 === 1;
+
+  const handlePreviousPhaseClick = () => {
+    setQuestion(0);
+    setPhase(phase - 1);
+  };
+
+  const handleNextPhaseClick = () => {
+    setQuestion(0);
+    setPhase(phase + 1);
+  };
+
+  const handlePreviousQuestionClick = () => {
+    if (question <= 0) {
+      handlePreviousPhaseClick();
+      return;
+    }
+
+    setQuestion(question - 1);
+  };
+
+  const handleNextQuestionClick = () => {
+    if (question >= questions.length - 1) {
+      handleNextPhaseClick();
+      return;
+    }
+
+    setQuestion(question + 1);
+  };
 
   const handleRavenOptionClick = (id: string) => {
     return () => setSelected(v => v === id ? "" : id);
@@ -48,12 +81,20 @@ export function RavenMatrixPage() {
       </div>
     </div>
     <div className={styles.phaseButtonsRow}>
-      <button className={styles.previousButton}>Previous phase</button>
-      <button className={styles.nextButton}>Next phase</button>
+      <button onClick={handlePreviousPhaseClick} disabled={phase === 0}>
+        Previous phase
+      </button>
+      <button onClick={handleNextPhaseClick} disabled={phase >= phases.length - 1}>
+        Next phase
+      </button>
     </div>
     <div className={styles.testButtonsRow}>
-      <button className={styles.previousButton}>Previous test</button>
-      <button className={styles.nextButton}>Next test</button>
+      <button onClick={handlePreviousQuestionClick} disabled={question === 0 && phase === 0}>
+        Previous test
+      </button>
+      <button onClick={handleNextQuestionClick}>
+        Next test
+      </button>
     </div>
   </div>;
 }
