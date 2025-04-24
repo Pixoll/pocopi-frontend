9ce008a5e8b678b4194b0933396d78c5ed15057f
Pixoll <pixoll.gamer@gmail.com>
2025-04-24 01:54:34 -0400

    feat: create basic raven matrix test

---
+++ apps/frontend/src/pages/RavenMatrixPage.tsx
@@ -0,0 +1,41 @@
+import { config } from "@pocopi/config";
+import { useState } from "react";
+import styles from "./RavenMatrixPage.module.css";
+
+export function RavenMatrixPage() {
+  const [selected, setSelected] = useState<string>("");
+
+  // TODO should be props
+  const { img, options: tempOptions } = config.protocols.control.phases[0];
+  const options = tempOptions.map(option => {
+    const base64 = option.src.split(";")[1].slice(7);
+    const half = Math.round(base64.length / 2);
+    const id = base64.substring(half - 10, half + 10);
+
+    return { id, ...option };
+  });
+  const isOdd = options.length % 2 === 1;
+
+  const handleRavenOptionClick = (id: string) => {
+    return () => setSelected(v => v === id ? "" : id);
+  };
+
+  return <div className={styles.ravenMatrixContainer}>
+    <img className={styles.ravenMatrix} src={img.src} alt={img.alt}></img>
+    <div className={styles.ravenOptionsContainer}>
+      {options.map((option, i) => (
+        <img
+          className={[
+            styles.ravenOption,
+            isOdd && i % 2 === 1 ? styles.oddRavenOption : "",
+            option.id === selected ? styles.selectedRavenOption : "",
+          ].join(" ")}
+          key={option.id}
+          src={option.src}
+          alt={option.alt}
+          onClick={handleRavenOptionClick(option.id)}
+        />
+      ))}
+    </div>
+  </div>;
+}
--- apps/frontend/src/routes/AppRoutes.tsx
+++ apps/frontend/src/routes/AppRoutes.tsx
@@ -1,10 +1,11 @@
+import { RavenMatrixPage } from "@/pages/RavenMatrixPage.tsx";
 import { Route, Routes } from "react-router";
 import { UserList } from "@/pages/UserList";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<UserList />} />
+      <Route path="/" element={<RavenMatrixPage />} />
       <Route path="/users" element={<UserList />} />
     </Routes>
   );
